6604aac06cdbdecba0c3180f64ddfeeec658dad9
Roberto Arcomano <info@robertoarcomano.com>
2023-05-06 23:20:33 +0200

    Added backup.sh

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SRC_FS=rpool/USERDATA/berto_zbq731
+DST_FS=backup/home_berto
+
+# Calc last snapshot
+OLD_SNAPSHOT=$(zfs list $SRC_FS -t snapshot -H|awk '{print $1}'|sort -r|head -1|awk -F '@' '{print $2}')
+NEW_SNAPSHOT="snapshot_"$(date +%Y%m%d_%H%M%S)
+sudo zfs snapshot $SRC_FS@$NEW_SNAPSHOT
+if [ "$OLD_SNAPSHOT" == "" ]; then
+  # No snapshot. I need to create the first one and do a full send/receive backup
+  sudo zfs send $SRC_FS@$NEW_SNAPSHOT | sudo zfs receive $DST_FS
+else
+  # At least 1 snapshot exists. I need to do an incremental backup
+  sudo zfs send -i $SRC_FS@$OLD_SNAPSHOT $SRC_FS@$NEW_SNAPSHOT | sudo zfs receive $DST_FS
+fi
--- test_backup.sh
+++ test_backup.sh
@@ -6,13 +6,9 @@ BACKUP_DIR1=/home/berto/Documents/moto
 SNAPSHOT_NAME=snapshot_test
 SNAPSHOT=$FS@$SNAPSHOT_NAME
 SNAPSHOT1=$FS@$SNAPSHOT_NAME"1"
-
 BACKUP_FS=backup/test
-BACKUP_MOUNTPOINT=/mnt/backup/test
-BACKUP_SNAPSHOT=$BACKUP_FS@$SNAPSHOT_NAME
 
 # 1 Remove old snapshot and fs
-#sudo zfs destroy $SNAPSHOT
 sudo zfs destroy $FS -r
 
 # 2 Create the local fs test
@@ -28,33 +24,23 @@ sudo rsync -a $BACKUP_DIR $MOUNTPOINT
 sudo zfs snapshot $SNAPSHOT
 
 # 6. Remove old backup snapshot and fs
-#sudo zfs destroy $BACKUP_SNAPSHOT
 sudo zfs destroy $BACKUP_FS -r
 
-# 7 Create the local fs test
-#sudo zfs create $BACKUP_FS
-
-# 8 Set the mountpoint to $BACKUP_MOUNTPOINT
-#sudo zfs set mountpoint=$BACKUP_MOUNTPOINT $BACKUP_FS
-
-# 9 Copy data to the backup using the send command
+# 7 Copy data to the backup using the send command
 echo "Full copy"
 date
 sudo zfs send $SNAPSHOT | sudo zfs receive $BACKUP_FS
 date
 echo
 
-# 10 Copy new data
+# 8 Copy new data
 sudo rsync -a $BACKUP_DIR1 $MOUNTPOINT
 
-# 11 Create new snapshot
+# 9 Create new snapshot
 sudo zfs snapshot $SNAPSHOT1
 
-# 12 Sync data to backup
+# 10 Sync data to backup
 echo "Incremental copy"
 date
 sudo zfs send -i $SNAPSHOT $SNAPSHOT1 | sudo zfs receive $BACKUP_FS
 date
-
-
-
